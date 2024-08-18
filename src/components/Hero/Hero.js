ba69236f2d6fbbc42bc40f56812df55d5592a272
Bharat Pahwa <pahwabharat15@gmail.com>
2024-08-18 18:52:49 +0530

    Urgent Email Back

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,56 @@
 import * as React from 'react';
 import { motion } from 'framer-motion';
 import heroImage from '../../assets/images/networking.png';
+import { useFirebase } from '../../contexts/Firebase';
+import { sendEmailToUser } from '../../MailService/waitlistMail';
 
 function HeroSection() {
+  const { handleAddUser } = useFirebase();
+  const [email, setEmail] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const [success, setSuccess] = React.useState(false);
+
+  const handleInputChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const validateEmail = (email) => {
+    // Regular expression for email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleJoinWaitlist = async () => {
+    setError(null);
+    setSuccess(false);
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address');
+      return;
+    }
+    setLoading(true);
+    try {
+      const res = await handleAddUser(email);
+      if (res === 'success') {
+        setSuccess(true);
+        setLoading(false);
+        const response = await sendEmailToUser(email);
+        if (response === 'success') {
+          alert('Mail sent successfully');
+        } else {
+          alert('Error sending welcome mail!');
+        }
+        setEmail('');
+      } else if (res === 'error') {
+        setError('Error Adding your Email! Please Try Again');
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div
       id='home'
@@ -20,33 +68,33 @@ function HeroSection() {
           Simplify your engagement journey using AI.
         </div>
         <div className='flex w-full flex-wrap mt-10 gap-5 justify-start sm:justify-start items-start self-start text-base font-medium tracking-normal leading-8'>
-          <div className='p-0 pl-0 w-full flex justify-start sm:justify-start flex-wrap rounded-full sm:py-0 items-start gap-4 lg:pl-0 sm:pr-5 text-sky-900'>
-            <a
-              href='https://chromewebstore.google.com/detail/ldhdipkofibjleihomflebfklhadikio?hl=en-GB&authuser=1'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <motion.button
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                className='whitespace-nowrap rounded-lg bg-sky-900 px-4 py-2 font-medium text-white shadow-xl transition-colors hover:bg-slate-700'
+          <div className='flex w-full flex-wrap gap-5 items-center justify-start text-base font-medium tracking-normal leading-8'>
+            <div className='p-0 pl-0 items-center flex-wrap rounded-full flex gap-4 lg:sm:xl:pl-0 sm:md:lg:xl:pr-5 text-sky-900'>
+              <input
+                type='email'
+                placeholder='Your Email Address'
+                value={email}
+                onChange={handleInputChange}
+                className='border sm:md:xl:w-[17rem] w-auto text-center font-medium hover:shadow-lg rounded-xl sm:lg:md:xl:pl-4 sm:lg:md:xl:text-left p-2 focus:outline-none'
+              />
+              <button
+                onClick={handleJoinWaitlist}
+                disabled={loading}
+                className={`justify-center shadow-xl rounded-xl p-2 pl-5 pr-5 text-white bg-sky-900 max-md:px-5 ${
+                  loading ? 'opacity-50 cursor-not-allowed' : 'hover:shadow-lg'
+                }`}
               >
-                Add to Chrome for Free
-              </motion.button>
-            </a>
-            <a
-              href='https://chat.whatsapp.com/CxMeAkMmAlnBJzVg89hkST'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <motion.button
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                className='whitespace-nowrap rounded-lg bg-sky-100 px-4 py-2 font-medium text-sky-900 transition-colors hover:text-sky-900  hover:border-2 hover:border-sky-900'
-              >
-                Join Community
-              </motion.button>
-            </a>
+                {loading ? 'Joining...' : 'Get Access'}
+              </button>
+            </div>
+            {error && (
+              <div className='text-red-500'>
+                Failed to add email... Try Again!!
+              </div>
+            )}
+            {success && (
+              <div className='text-green-500'>Email added successfully!</div>
+            )}
           </div>
         </div>
       </div>
