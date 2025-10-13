52782dffa181731c7b3e0292ea79a79d046150d8
Bharat Pahwa <letsbunktoday@gmail.com>
2025-10-14 01:57:56 +0530

    Update OrganizationCard.js

--- src/components/Dashboard/Sidebar/OrganizationCard.js
+++ src/components/Dashboard/Sidebar/OrganizationCard.js
@@ -6,7 +6,6 @@ import UpgradeModal from '../../Common/UpgradeModal';
 import CommandPalette from '../Global/CommandPalette';
 import CreditsConsumptionModal from '../Global/CreditsConsumptionModal';
 
-
 const OrganizationCard = ({
   userData,
   isOpen,
